11772df4b9e25f3c791626d95eeeaff911f0b134
DESKTOP-T6LJGMF\hoant <hoanguyenthi210303@gmail.com>
2023-06-11 01:36:29 +0700

    tạo cấu trúc prj reactJs

---
+++ src/view/App.js
@@ -0,0 +1,11 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">{/* <Hoant></Hoant> */}</header>
+    </div>
+  );
+}
+
+export default App;
